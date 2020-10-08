33a79742cdc7b859ab4351fc559d313603e0a40f
vidal987 <mvsouza.af@gmail.com>
2020-10-08 19:59:57 -0300

    add: pasta convertendo tipos

---
+++ ConvertentoTipos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConvertentoTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            //real = inteiro; //100
+            //inteiro = (int)real;
+            //string valorReal = real.ToString();
+            //inteiro = int.Parse(valorReal);
+
+            inteiro = Convert.ToInt32(real);
+
+            //Console.WriteLine(inteiro);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(Convert.ToBoolean(255));
+
+
+        }
+    }
+}
