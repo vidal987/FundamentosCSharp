0aa9ec24c6fa38c55efa13c9a79432c18d4358ec
vidal987 <mvsouza.af@gmail.com>
2020-10-09 09:20:58 -0300

    add: value types Reference Type

---
+++ valueTypes-ReferenceTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace valueTypes_ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //os inteiros fazem uma copia do item na memoria
+            int x = 25;
+            int y = x;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            x = 32;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+
+            var arr = new string[2];
+            arr[0] = "Item 1";
+
+            var arr2 = arr;
+
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+
+            //os arrays só tem referencia do dado
+            arr[0] = "Item 2";
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr2[0]);
+
+        }
+    }
+}
