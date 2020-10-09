97b8e4d57c1a01ffea81f07cf5fd18bb68cff36e
vidal987 <mvsouza.af@gmail.com>
2020-10-09 09:21:19 -0300

    add: Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //enumerador referencia a um metodo ou uma referencia, tipo a interface
+        enum EproductType
+        {
+            Product = 1,
+            Service = 2
+        }
+
+
+
+    }
+}
