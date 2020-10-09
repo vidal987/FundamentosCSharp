8fffe8e8ea50b434632ef04d89260020023dca23
vidal987 <mvsouza.af@gmail.com>
2020-10-09 09:21:35 -0300

    add: struct

---
+++ structs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Mouse gamer", 299.97);
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+
+        }
+
+        struct Product
+        {
+            public Product(int id, string name, double price)
+            {
+                Id = id;
+                Name = name;
+                Price = price;
+            }
+
+            public int Id;
+            public string Name;
+            public double Price;
+
+            public double PricInDolar(double dolar)
+            {
+                return Price * dolar;
+            }
+        }
+    }
+}
