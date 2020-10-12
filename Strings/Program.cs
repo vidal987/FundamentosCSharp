27ea1cf2867bbaa496c0411a6234751e049a75bb
vidal987 <mvsouza.af@gmail.com>
2020-10-12 11:24:35 -0300

    add: strings

---
+++ Strings/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // //guid gerador de hashs
+            // var id = Guid.NewGuid();
+            // id.ToString();
+
+            // id = new Guid("");
+            // Console.WriteLine(id.ToString().Substring(0, 8));
+
+
+            // // interpolação de strings
+            // var price = 10.2;
+            // // var texto = "O preço do produto é" + price;
+            // // var texto = string.Format("O PREÇO DO PRODUTO É {0}" + price);
+            // // var texto = $"o preço do produto é {price}";
+
+
+            // //COMPARÇÃO DE STRING
+            // var texto = "Testando";
+            // Console.WriteLine(texto.CompareTo("Testando"));
+            // Console.WriteLine(texto.CompareTo("testando"));
+            // Console.WriteLine(texto.Contains("Teste"))
+            // Console.WriteLine(texto.Contains("teste", StringComparison.OrdinalIgnoreCase));
+
+            // var texto = "Este texto é um teste";
+            // Console.WriteLine(texto.StartsWith("Este"));
+            // Console.WriteLine(texto.StartsWith("este"));
+            // Console.WriteLine(texto.StartsWith("texto"));
+
+            // Console.WriteLine(texto.EndsWith("teste"));
+            // Console.WriteLine(texto.EndsWith("Teste"));
+            // Console.WriteLine(texto.EndsWith("Tste"));
+
+
+            // //EQUALS PARA COMPARAR QUALQUER OBJETO
+            // var texto = "texto";
+            // Console.WriteLine(texto.Equals("texto"));
+            // Console.WriteLine(texto.Equals("Texto"));
+
+
+            // // Indices
+
+            // var texto = "Este texto é um teste";
+            // //primeira letra
+            // Console.WriteLine(texto.IndexOf("é"));
+            // //ultima letra
+            // Console.WriteLine(texto.LastIndexOf("s"));
+
+            // //METODOS ADICIONAIS
+            // //TOlOWER == CONVERTER PRA MINUSCULO
+
+            // //TOuPPER == CONVERTER PARA MAIUSCULO
+
+            // //INSERT == INSERIR TEXTO, 
+
+            // //REMOVE == REMOVER O TEXTO
+
+            // //LENGTH == TAMANHO OU QUANTIDADE DE CARACTERES
+
+
+            //MANIPULANDO STRINGS
+
+            //REPLACE TROCA A PALAVRA, LOCALIZA ELA COM A CHAVE E TROCA POR OUTRO VALOR
+            // SLIT == DIVIDIR TEXTO EM VARIOS
+
+            //SUBSTRING == PEGAR CARACTERES APARTIR DE UMA CHAVE VALOR
+
+            //TRIM REMOVE ESPAÇOS 
+
+            //STRING BUILDER
+
+            // var texto = new StringBuilder();
+
+            // texto.Append("este textp é um teste");
+        }
+    }
+}
